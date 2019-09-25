f5fff174a41baaeda2a5439a67ef14a388ebf1f7
Melanie Rogan <melanie.rogan@ft.com>
2019-09-25 11:59:50 +0100

    more workings

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+const fs = require('fs');
+// const contents = fs.readFileSync('./lib/non_friendly.json');
+// const jsonContent = JSON.parse(contents);
+
+
 /**
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Application} app
  */
 module.exports = app => {
+	
 	app.log('Yay, the app was loaded!');
 
 	app.on('pull_request', async context => {
@@ -12,6 +18,10 @@ module.exports = app => {
 
 		const isBumcomment = context.issue({
 			body: 'This PR contains a bum',
+		});
+
+		const isUnfriendlyComment = context.issue({
+			body: 'This PR contains some non inclusive or unfriendly terms',
 		});
 		const owner = context.payload.repository.owner.login;
 		const repo = context.payload.repository.name;
@@ -35,13 +45,29 @@ module.exports = app => {
 			number,
 		});
 		const checkCommit = files.data[0].patch.split('\n');
-		const fullcheck = checkCommit.some(function(x) {
-			return x === '+bum';
-		});
-		console.log(fullcheck, 'checking the test for the word with some');
-		console.log(checkCommit.includes('bum'), 'checking the test for the word');
-		if (fullcheck) {
-			context.github.issues.createComment(isBumcomment);
+		console.log(checkCommit, 'the checkcommit');
+		const unfriendlyWordsHere = require('./lib/non_friendly');
+		// const isUnfriendly = (unfriendlyWordsHere, checkCommit) => {
+		// 	return unfriendlyWordsHere.some(item => item === checkCommit);
+		// };
+		const b = ['+bum', '+blacklist'];
+		console.log(checkCommit.some(v => b.includes(v)));
+		const checkerFinal = checkCommit.some(v => unfriendlyWordsHere.includes(v));
+
+		// console.log(
+		// 	isUnfriendly(jsonContent, checkCommit),
+		// 	'is this going to find the things?',
+		// );
+		// console.log(jsonContent.words, 'maybe the words');
+
+		// const fullcheck = checkCommit.some(function(x) {
+		// 	return x === jsonContent;
+		// });
+		// console.log(jsonContent, 'what json content');
+		// console.log(fullcheck, 'checking the test for the word with some');
+		// console.log(checkCommit.includes('bum'), 'checking the test for the word');
+		if (checkerFinal) {
+			context.github.issues.createComment(isUnfriendlyComment);
 		}
 	});
 };
