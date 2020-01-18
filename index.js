994f353cf7c5d230ab9cc009b87a427142b05d32
Melanie Rogan <melanie.rogan@ft.com>
2020-01-18 15:59:02 +0000

    now why isn't the issue posting

--- index.js
+++ index.js
@@ -73,10 +73,11 @@ module.exports = app => {
 			You the following words were used: **${wordsFound}**.
 		  These words were found on the following lines: **${linesFound}**.`,
 		});
-
+		console.log(result, 'result');
+		console.log(result[0], 'what now');
+		console.log(result[0].status, 'what do i get here for status');
 		if (result[0].status) {
 			context.github.issues.createComment(isUnfriendlyComment);
-			console.log('ISSUE POSTED');
 		}
 	});
 };
